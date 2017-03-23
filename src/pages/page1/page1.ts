a56a534e8ec7140940888f58e9f7aeeae41b0227
CaioCris <caiohcris@gmail.com>
2017-03-23 18:33:02 -0300

    foto ok

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -9,6 +9,7 @@ import { Camera } from 'ionic-native';
 })
 export class Page1 {
   public base64Image: string;
+  public images: string[] = [];
 
   constructor(
     public navCtrl: NavController,
@@ -22,6 +23,8 @@ export class Page1 {
     }).then((imageData) => {
       // imageData is a base64 encoded string
         this.base64Image = "data:image/jpeg;base64," + imageData;
+        this.images.push(this.base64Image);
+        console.log(this.images);
     }, (err) => {
         console.log(err);
     });
