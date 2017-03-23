f8e2e005a4ebdd24bc9c4ed443477cbe26d9e31f
Flavio Brognara <flavio.brognara@fcamara.com.br>
2017-03-23 16:54:51 -0300

    .

--- src/pages/camera-teste/camera-teste.ts
+++ src/pages/camera-teste/camera-teste.ts
@@ -17,8 +17,8 @@ export class CameraTestePage {
  takePicture(){
     Camera.getPicture({
         destinationType: Camera.DestinationType.DATA_URL,
-        targetWidth: 1000,
-        targetHeight: 1000
+        targetWidth: 150,
+        targetHeight: 150
     }).then((imageData) => {
       // imageData is a base64 encoded string
         this.base64Image = "data:image/jpeg;base64," + imageData;
