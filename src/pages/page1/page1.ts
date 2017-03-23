1f676cfbeac41af14a1968c4aa266e353f8966be
CaioCris <caiohcris@gmail.com>
2017-03-23 16:56:35 -0300

    SE FUDE

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -17,8 +17,8 @@ export class Page1 {
  takePicture(){
     Camera.getPicture({
         destinationType: Camera.DestinationType.DATA_URL,
-        targetWidth: 1000,
-        targetHeight: 1000
+        targetWidth: 200,
+        targetHeight: 200
     }).then((imageData) => {
       // imageData is a base64 encoded string
         this.base64Image = "data:image/jpeg;base64," + imageData;
