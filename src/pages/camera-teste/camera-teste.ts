694a257d759aefba622190c34c8156e2589c1103
Flavio Brognara <flavio.brognara@fcamara.com.br>
2017-03-28 13:02:22 -0300

    Notifications working

--- src/pages/camera-teste/camera-teste.ts
+++ src/pages/camera-teste/camera-teste.ts
@@ -15,36 +15,5 @@ export class CameraTestePage {
     public navCtrl: NavController,
     public navParams: NavParams){}
     
-
-  notifyMe() {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
-
-  // At last, if the user has denied notifications, and you 
-  // want to be respectful there is no need to bother them any more.
-}Notification.requestPermission();function spawnNotification(corpo,icone,titulo) {
-  var opcoes = {
-      body: corpo,
-      icon: icone
-  }
-  var n = new Notification(titulo,opcoes);
 }
-}
+
