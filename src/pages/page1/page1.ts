d73bffb781316b95d63e085799836681dcd4b251
Christian <cbenseler@gmail.com>
2017-03-29 15:00:21 -0300

    refactor: using viewchild to get elements

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 
@@ -10,6 +10,8 @@ export class Page1 {
   public images: string[] = [];
   public cards: string[] = [];
 
+  @ViewChild('video') video:ElementRef;
+  @ViewChild('canvas') canvas:ElementRef;
 
 
   constructor(
@@ -18,29 +20,22 @@ export class Page1 {
   }
 
   openCamera() {
-    // Grab elements, create settings, etc.
-    var video = <any>document.getElementById('video');
 
     // Get access to the camera!
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
+      navigator.mediaDevices.getUserMedia({ video: true }).then( stream => {
+        this.video.nativeElement.src = window.URL.createObjectURL(stream);
+        this.video.nativeElement.play();
       });
     }
   }
 
   takePicture() {
     // Elements for taking the snapshot
-    var canvas = <any> document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var video = document.getElementById('video');
-
-    // Trigger photo take
-    document.getElementById("snap").addEventListener("click", function () {
-      context.drawImage(video, 0, 0, 640, 480);
-    });
+    let context = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(<HTMLVideoElement>this.video.nativeElement, 0, 0, 640, 480);
+    
   }
 
 }
